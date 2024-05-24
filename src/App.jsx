5447d28368dbfa422e2d6162602daf941d0c60de
Ace <ace.lowder@gmail.com>
2024-05-23 22:08:42 -0700

    Add flexbox to footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,33 @@ export default function App() {
   return (
     <>
       <main>
-        <img src="spiralgalaxy.jpg" className="bg-image"/>
+        <img src="spiralgalaxy.jpg" className="bg-image" />
         <div className="bg-gradient"></div>
       </main>
       <div>
         <h2>Spiral Galaxy</h2>
         <p>Description</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, quos ipsa aliquam mollitia ducimus inventore nesciunt incidunt, veniam illo, voluptas quas atque repellendus distinctio quisquam? Quam assumenda nesciunt atque quibusdam.
-        Dicta ullam esse sed error quisquam veritatis odio architecto similique id repellat atque blanditiis nostrum, ipsam dignissimos ex aliquam corrupti at, deleniti molestias. Magnam eveniet temporibus aliquid? Itaque, deleniti magni!
-        Itaque possimus quod, id vitae dicta minima debitis dolore voluptatem, nisi expedita molestias deserunt quibusdam vel assumenda aspernatur optio delectus quasi, doloremque explicabo cumque accusantium nulla. Aspernatur placeat veniam atque?</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, quos
+          ipsa aliquam mollitia ducimus inventore nesciunt incidunt, veniam
+          illo, voluptas quas atque repellendus distinctio quisquam? Quam
+          assumenda nesciunt atque quibusdam. Dicta ullam esse sed error
+          quisquam veritatis odio architecto similique id repellat atque
+          blanditiis nostrum, ipsam dignissimos ex aliquam corrupti at, deleniti
+          molestias. Magnam eveniet temporibus aliquid? Itaque, deleniti magni!
+          Itaque possimus quod, id vitae dicta minima debitis dolore voluptatem,
+          nisi expedita molestias deserunt quibusdam vel assumenda aspernatur
+          optio delectus quasi, doloremque explicabo cumque accusantium nulla.
+          Aspernatur placeat veniam atque?
+        </p>
       </div>
       <footer>
-        <h2>Spiral Galaxy</h2>
-        <h1>APOD Project</h1>
+        <div>
+          <h2>Spiral Galaxy</h2>
+          <h1>APOD Project</h1>
+        </div>
         <button>i</button>
       </footer>
     </>
-  )
+  );
 }
